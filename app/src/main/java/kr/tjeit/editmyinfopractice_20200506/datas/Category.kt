4bd1abf8a702d9348a0174e33d97c0cadecdb3e6
cho881020 <kj_cho@idealidea.co.kr>
2020-05-06 21:12:02 +0900

    사용자/카테고리 데이터클래스 생성

---
+++ app/src/main/java/kr/tjeit/editmyinfopractice_20200506/datas/Category.kt
@@ -0,0 +1,9 @@
+package kr.tjeit.editmyinfopractice_20200506.datas
+
+class Category {
+
+    var id = 0
+    var title = ""
+    var color = ""
+
+}
---
+++ app/src/main/java/kr/tjeit/editmyinfopractice_20200506/datas/User.kt
@@ -0,0 +1,16 @@
+package kr.tjeit.editmyinfopractice_20200506.datas
+
+import java.util.*
+
+class User {
+
+    var id = 0
+    var loginId = ""
+    var name = ""
+    var phoneNum = ""
+    var memo = ""
+
+    var createdAt = Calendar.getInstance()
+    var category = Category()
+
+}
