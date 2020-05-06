30f29c6a2a08ef1c117424dc8596ddc57f6cb0bd
cho881020 <kj_cho@idealidea.co.kr>
2020-05-06 20:38:07 +0900

    어댑터 / 스피너 연결  => 기본 뷰는 나오는데, 클릭하면 앱이 죽는 상태

--- app/src/main/java/kr/tjeit/editmyinfopractice_20200506/MainActivity.kt
+++ app/src/main/java/kr/tjeit/editmyinfopractice_20200506/MainActivity.kt
@@ -7,8 +7,13 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kr.tjeit.editmyinfopractice_20200506.adapters.PizzaStoreAdapter
+import kr.tjeit.editmyinfopractice_20200506.datas.PizzaStore
 
 class MainActivity : BaseActivity() {
+
+    lateinit var myAdapter : PizzaStoreAdapter
+    val pizzaStores = ArrayList<PizzaStore>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,6 +53,18 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        addPizzaStores()
+
+        myAdapter = PizzaStoreAdapter(mContext, R.layout.pizza_store_item, pizzaStores)
+        pizzaSpinner.adapter = myAdapter
+    }
+
+    private fun addPizzaStores() {
+
+        pizzaStores.add(PizzaStore("피자헛", "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg"))
+        pizzaStores.add(PizzaStore("파파존스", "http://mblogthumb2.phinf.naver.net/20160530_65/ppanppane_1464617766007O9b5u_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%284%29.png?type=w800"))
+        pizzaStores.add(PizzaStore("미스터피자", "https://post-phinf.pstatic.net/MjAxODEyMDVfMzYg/MDAxNTQzOTYxOTA4NjM3.8gsStnhxz7eEc9zpt5nmSRZmI-Pzpl4NJvHYU-Dlgmcg.7Vpgk0lopJ5GoTav3CUDqmXi2-_67S5AXD0AGbbR6J4g.JPEG/IMG_1641.jpg?type=w1200"))
+        pizzaStores.add(PizzaStore("도미노피자", "https://pbs.twimg.com/profile_images/1098371010548555776/trCrCTDN_400x400.png"))
 
     }
 
