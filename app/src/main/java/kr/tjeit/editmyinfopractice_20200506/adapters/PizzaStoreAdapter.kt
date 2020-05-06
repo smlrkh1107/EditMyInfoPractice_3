26d4feef76f6b11edfea856d0860a88dbaf51c5a
cho881020 <kj_cho@idealidea.co.kr>
2020-05-06 20:20:02 +0900

    어댑터 기초 작업

---
+++ app/src/main/java/kr/tjeit/editmyinfopractice_20200506/adapters/PizzaStoreAdapter.kt
@@ -0,0 +1,31 @@
+package kr.tjeit.editmyinfopractice_20200506.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import kr.tjeit.editmyinfopractice_20200506.R
+import kr.tjeit.editmyinfopractice_20200506.datas.PizzaStore
+
+class PizzaStoreAdapter(val mContext: Context, resId:Int, val mList:ArrayList<PizzaStore>) : ArrayAdapter<PizzaStore>(mContext, resId, mList) {
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        tempRow?.let {
+
+        }.let {
+            tempRow = inf.inflate(R.layout.pizza_store_item, null)
+        }
+
+        val row = tempRow!!
+
+//        실제 데이터 반영 코드 작업 필요
+
+        return row
+    }
+
+
+}
