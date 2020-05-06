c821a55a63e71e97a0b0183c1e279c62a3d1d03b
cho881020 <kj_cho@idealidea.co.kr>
2020-05-06 19:38:47 +0900

    Initial commit

---
+++ app/src/main/java/kr/tjeit/editmyinfopractice_20200506/MainActivity.kt
@@ -0,0 +1,23 @@
+package kr.tjeit.editmyinfopractice_20200506
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
