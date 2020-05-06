ca0f679dd6f4239753ec9903111a1484d66a90fb
cho881020 <kj_cho@idealidea.co.kr>
2020-05-06 19:57:16 +0900

    확인버튼 누르면 스피너 선택값 가져오기

--- app/src/main/java/kr/tjeit/editmyinfopractice_20200506/MainActivity.kt
+++ app/src/main/java/kr/tjeit/editmyinfopractice_20200506/MainActivity.kt
@@ -2,6 +2,8 @@ package kr.tjeit.editmyinfopractice_20200506
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
@@ -13,6 +15,15 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        okBtn.setOnClickListener {
+//            스피너에 선택된 문구를 로그로 출력.
+            val selectedJob = jobSpinner.selectedItem as String
+            Log.d("선택된 직업", selectedJob)
+
+            val selectedPosition = jobSpinner.selectedItemPosition
+            Log.d("선택된 위치", "${selectedPosition}번째 아이템")
+        }
 
     }
 
