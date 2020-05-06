2fabe675d15b7d3934a710277f0c7dfde33115f1
cho881020 <kj_cho@idealidea.co.kr>
2020-05-06 22:03:04 +0900

    유져 / 카테고리 파싱 기능 생성  => 유져의 가입일시가 아직 파싱 안됨

--- app/src/main/java/kr/tjeit/editmyinfopractice_20200506/datas/Category.kt
+++ app/src/main/java/kr/tjeit/editmyinfopractice_20200506/datas/Category.kt
@@ -1,6 +1,18 @@
 package kr.tjeit.editmyinfopractice_20200506.datas
 
+import org.json.JSONObject
+
 class Category {
+
+    companion object {
+        fun getCategoryFromJson(json: JSONObject) : Category {
+            val ct = Category()
+            ct.id = json.getInt("id")
+            ct.title = json.getString("title")
+            ct.color = json.getString("color")
+            return ct
+        }
+    }
 
     var id = 0
     var title = ""
--- app/src/main/java/kr/tjeit/editmyinfopractice_20200506/datas/User.kt
+++ app/src/main/java/kr/tjeit/editmyinfopractice_20200506/datas/User.kt
@@ -1,8 +1,26 @@
 package kr.tjeit.editmyinfopractice_20200506.datas
 
+import org.json.JSONObject
 import java.util.*
 
 class User {
+
+    companion object {
+        fun getUserFromJsonObject(json: JSONObject) : User {
+            val parsedUser = User()
+            parsedUser.id = json.getInt("id")
+            parsedUser.loginId = json.getString("login_id")
+            parsedUser.name = json.getString("name")
+            parsedUser.phoneNum = json.getString("phone")
+            parsedUser.memo = json.getString("memo")
+
+            parsedUser.category = Category.getCategoryFromJson(json.getJSONObject("category"))
+
+
+
+            return parsedUser
+        }
+    }
 
     var id = 0
     var loginId = ""
