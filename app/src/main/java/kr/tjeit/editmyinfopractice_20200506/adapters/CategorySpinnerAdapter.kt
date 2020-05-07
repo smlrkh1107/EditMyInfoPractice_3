0353a1c0d1ff2402f043c6fac3626cc20de7dfbc
smlrkh1107 <krae7789230@gmail.com>
2020-05-07 21:04:23 +0900

    스피너 실제 값 반영 =>String을 가지고 Color로 변환해서 활용

--- app/src/main/java/kr/tjeit/editmyinfopractice_20200506/adapters/CategorySpinnerAdapter.kt
+++ app/src/main/java/kr/tjeit/editmyinfopractice_20200506/adapters/CategorySpinnerAdapter.kt
@@ -1,11 +1,14 @@
 package kr.tjeit.editmyinfopractice_20200506.adapters
 
 import android.content.Context
+import android.graphics.Color
 import android.icu.util.ULocale
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import kr.tjeit.editmyinfopractice_20200506.R
 import kr.tjeit.editmyinfopractice_20200506.datas.Category
 
@@ -15,6 +18,8 @@ class CategorySpinnerAdapter(val mContext: Context, val resId:Int, val mList:Arr
 
     val inf = LayoutInflater.from(mContext)
 
+    
+//    선택된거 보여줌
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         var tempRow = convertView
         tempRow?.let {
@@ -22,10 +27,23 @@ class CategorySpinnerAdapter(val mContext: Context, val resId:Int, val mList:Arr
         }.let {
             tempRow = inf.inflate(R.layout.category_list_item, null)
         }
+
         val row = tempRow!!
+        val colorImg = row.findViewById<ImageView>(R.id.colorImg)
+        val titleTxt = row.findViewById<TextView>(R.id.titleTxt)
+
+        val data = mList.get(position)
+
+        titleTxt.text = data.title
+//        kotlin set background color ???(R.color.green) [ 구글링]
+
+        colorImg.setBackgroundColor(Color.parseColor(data.color))
+
+
         return row
     }
 
+//    목록에 뿌려짐
     override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
         var tempRow = convertView
         tempRow?.let {
@@ -33,7 +51,19 @@ class CategorySpinnerAdapter(val mContext: Context, val resId:Int, val mList:Arr
         }.let {
             tempRow = inf.inflate(R.layout.category_list_item, null)
         }
+
         val row = tempRow!!
+        val colorImg = row.findViewById<ImageView>(R.id.colorImg)
+        val titleTxt = row.findViewById<TextView>(R.id.titleTxt)
+
+        val data = mList.get(position)
+
+        titleTxt.text = data.title
+//        kotlin set background color ???(R.color.green) [ 구글링]
+
+        colorImg.setBackgroundColor(Color.parseColor(data.color))
+
+
         return row
 
     }
