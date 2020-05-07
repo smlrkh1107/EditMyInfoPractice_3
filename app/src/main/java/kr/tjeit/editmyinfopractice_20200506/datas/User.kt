e9965debd5cc20b68f24a30a1bcb0a7f3113f881
cho881020 <kj_cho@idealidea.co.kr>
2020-05-07 19:54:33 +0900

    서버에서 알려주는 가입일시를 Calendar로 변환 해서 저장

--- app/src/main/java/kr/tjeit/editmyinfopractice_20200506/datas/User.kt
+++ app/src/main/java/kr/tjeit/editmyinfopractice_20200506/datas/User.kt
@@ -1,6 +1,7 @@
 package kr.tjeit.editmyinfopractice_20200506.datas
 
 import org.json.JSONObject
+import java.text.SimpleDateFormat
 import java.util.*
 
 class User {
@@ -15,6 +16,16 @@ class User {
             parsedUser.memo = json.getString("memo")
 
             parsedUser.category = Category.getCategoryFromJson(json.getJSONObject("category"))
+
+            val createdAtStr = json.getString("created_at")
+
+//            서버의 가입일시 : String > 앱의 가입일시 : Calendar
+//            양식 가공을 SimpleDateFormat으로 진행.
+
+            val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+
+//            가입일시의 값을 Date타입으로 변환한 객체로 대입.
+            parsedUser.createdAt.time = sdf.parse(createdAtStr)
 
 //            parsedUser.createdAt.set()
 
